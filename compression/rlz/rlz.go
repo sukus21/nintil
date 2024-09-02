d5d188ed5c252a03085b8cb236142400a918803c
sukus <sukus21@gmail.com>
2024-09-02 20:26:59 +0200

    rlz: validate data length

--- compression/rlz/rlz.go
+++ compression/rlz/rlz.go
@@ -2,8 +2,11 @@ package rlz
 
 import (
 	"bytes"
+	"errors"
 	"io"
 )
+
+var ErrRlzSize = errors.New("rlz: decompressed data size differs from size specified in header")
 
 // Reads a compressed stream of RLZ compressed data.
 // To learn more about the format, check the README file.
@@ -16,11 +19,23 @@ type RLZReader interface {
 }
 
 // Decompresses a blob of RLZ compressed data.
+// As a bonus, this method validates the size of the output data.
 func Decompress(dat []byte) ([]byte, error) {
 	buf := bytes.NewReader(dat)
 	dec, err := NewReader(buf)
 	if err != nil {
 		return nil, err
 	}
-	return io.ReadAll(dec)
+
+	// Decompress all data
+	out, err := io.ReadAll(dec)
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate size
+	if dec.Size() != len(out) {
+		return out, ErrRlzSize
+	}
+	return out, nil
 }
