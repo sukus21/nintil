cf8131493a0ffc071650a8164c5e4c22de0faae2
sukus <sukus21@gmail.com>
2024-09-03 22:02:52 +0200

    dat: update function signatures AGAIN

--- nds/pit/dat.go
+++ nds/pit/dat.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-
-	"github.com/sukus21/nintil/util/ezbin"
 )
 
 // Open a dat file from a reader
@@ -22,45 +20,30 @@ func OpenDat(r io.ReaderAt, index int) (*io.SectionReader, error) {
 	), nil
 }
 
-// Get number of dats inside a single file
-func NumDats(r io.ReaderAt, length int) int {
-	var offset, previous uint32
-	for i := 0; ; i += 4 {
-		if err := ezbin.ReadAt(r, i, &offset); err != nil {
-			return 0
-		}
-
-		// Not a .dat file
-		if offset < previous {
-			return 0
-		}
-
-		if offset == uint32(length) {
-			return i << 2
-		}
-	}
-}
-
-// Check if a binary blob is a .dat file
-func IsDat(dat []byte) bool {
-	first := 0
-	prev := 0
-	for i := 0; i+3 < len(dat); i += 4 {
-		offset := int(binary.LittleEndian.Uint32(dat[i : i+4]))
+// Read the file offsets in a .dat file
+func DatOffsets(dat []byte) []uint32 {
+	offsets := make([]uint32, 0, 32)
+	first := uint32(0)
+	prev := uint32(0)
+	for i := uint32(0); i+3 < uint32(len(dat)); i += 4 {
+		offset := binary.LittleEndian.Uint32(dat[i : i+4])
+		offsets = append(offsets, offset)
 
 		// Full file contents used, is a .dat file
 		if i >= 4 && int(offset) == len(dat) {
-			return i+4 == first
+			if i+4 == first {
+				return offsets
+			}
 		}
 
 		// Entry beyond file length, not a .dat file
 		if int(offset) > len(dat) {
-			return false
+			return nil
 		}
 
 		// Entry did not come in sequence, not a .dat
 		if offset < prev && prev != 0 {
-			return false
+			return nil
 		}
 		prev = offset
 		if i == 0 {
@@ -69,22 +52,24 @@ func IsDat(dat []byte) bool {
 	}
 
 	// Abrupt end, not a .dat file
-	return false
+	return nil
+}
+
+// Get number of dats inside a single file
+func NumDats(dat []byte) int {
+	return max(0, len(DatOffsets(dat))/4-1)
+}
+
+// Check if a binary blob is a .dat file
+func IsDat(dat []byte) bool {
+	return NumDats(dat) != 0
 }
 
 // Unpack contents of .dat files
 func UnpackDat(dat []byte) [][]byte {
 
 	// Get list of offsets
-	offsets := make([]uint32, 0)
-	l := len(dat)
-	for i := 0; ; i += 4 {
-		o := binary.LittleEndian.Uint32(dat[i : i+4])
-		offsets = append(offsets, o)
-		if o == uint32(l) {
-			break
-		}
-	}
+	offsets := DatOffsets(dat)
 
 	// Read all file contents
 	files := make([][]byte, 0)
