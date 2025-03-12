7231130184c95469fb634c4a3451dbf836a75b12
sukus <52664542+sukus21@users.noreply.github.com>
2025-03-12 19:10:26 +0100

    pit: improve examples

--- example/nds/pit/fmap/README.md
+++ example/nds/pit/fmap/README.md
@@ -3,4 +3,4 @@
 Exports all overworld maps from a provided `FMapData.dat` file and `FMapInfo` block.
 
 ## Usage:
-`go run github.com/sukus21/nintil/example/nds/pit/fmap <FMapInfo block file path> <FMapData.dat path>`
+`go run github.com/sukus21/nintil/example/nds/pit/fmap <PiT ROM path>`
--- example/nds/pit/fmap/main.go
+++ example/nds/pit/fmap/main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"image/png"
+	"io"
 	"os"
+	"slices"
 
+	"github.com/sukus21/nintil/nds"
 	"github.com/sukus21/nintil/nds/pit"
 	"github.com/sukus21/nintil/util"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("usage:", os.Args[0], "<FMapInfo block file path>", "<FMapData.dat path>")
+	if len(os.Args) != 2 {
+		fmt.Println("usage:", os.Args[0], "<PiT ROM path>")
 		return
 	}
 
-	fmapInfo := util.Must1(os.ReadFile(os.Args[1]))
-	fmapFile := util.Must1(os.Open(os.Args[2]))
-	fmapReader := util.Must1(pit.NewFMapReader(fmapInfo, fmapFile))
+	// Open ROM file
+	romFile := util.Must1(os.Open(os.Args[1]))
+	rom := util.Must1(nds.OpenROM(romFile))
+
+	// Find FMapInfo
+	fmapInfo := findFmapInfo(rom)
+	fmapFile := util.Must1(rom.Filesystem.Open("FMap/FMapData.dat"))
+	fmapReader := util.Must1(pit.NewFMapReader(fmapInfo, bytes.NewReader(util.Must1(io.ReadAll(fmapFile)))))
 
 	for i := range fmapReader.MapCount() {
 		os.RemoveAll(fmt.Sprintf("FMap_%d.png", i))
@@ -29,6 +38,17 @@ func main() {
 			fmt.Println(err)
 		}
 	}
+}
+
+func findFmapInfo(rom *nds.Rom) []byte {
+	size := 638 * 20
+	for i := range rom.Arm9Binary[:len(rom.Arm9Binary)-size] {
+		if slices.Equal([]byte{0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0}, rom.Arm9Binary[i:i+16]) {
+			return rom.Arm9Binary[i : i+size]
+		}
+	}
+
+	panic("could not find the thing :(")
 }
 
 func RenderMap(fmapReader *pit.FMapReader, mapId int) error {
@@ -46,6 +66,20 @@ func RenderMap(fmapReader *pit.FMapReader, mapId int) error {
 	err = exportImage(img, fmt.Sprintf("FMap_%d.png", mapId))
 	if err != nil {
 		return err
+	}
+
+	// Render layers
+	for i := range 3 {
+		if renderer.HasLayer(i) {
+			img, err := renderer.RenderLayer(i)
+			if err != nil {
+				return err
+			}
+			err = exportImage(img, fmt.Sprintf("FMap_%d_%d.png", mapId, i))
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	// All good
