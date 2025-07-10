f13cef53453ec539d98d36f8efb83e8c320e3ae4
sukus <sukus21@gmail.com>
2025-07-10 16:49:03 +0200

    ezbin: overhaul decoder
    It is now easier to work with :)

--- util/ezbin/decode.go
+++ util/ezbin/decode.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"io"
+	"maps"
 	"reflect"
 	"strconv"
 	"strings"
@@ -12,39 +13,16 @@ import (
 	"github.com/sukus21/nintil/util"
 )
 
-func Decode[T any](src io.Reader) (v T, err error) {
-	defer util.Recover(&err)
-
-	r := &EndianedReader{
-		Reader:    src,
-		ByteOrder: getEndian(src),
-	}
-
-	// Simple things
-	t := reflect.New(reflect.TypeFor[T]()).Elem()
-	decodeValue(t, r, defaultTags)
-	v = t.Interface().(T)
-	return
-}
-
 const tagLength = "ezbin_length"
 const tagString = "ezbin_string"
 const tagByteorder = "ezbin_byteorder"
-const tagSeekpos = "ezbin_seekpos"
 const tagFixedPoint = "ezbin_fixedpoint"
-
-type decodeTags struct {
-	length  int
-	xstring string
-	fixed   string
-}
-
-var defaultTags = decodeTags{
-	length: -1,
-}
+const tagTell = "ezbin_tell"
+const tagOffset = "ezbin_offset"
+const tagOffsetArray = "ezbin_offset_array"
 
 var ErrSliceMissingLength = errors.New("cannot decode slice without length tag")
-var ErrUnknownLengthProperty = errors.New("field specified in length tag is not yet set or does not exist")
+var ErrUnknownNamedInt = errors.New("malformed int or invalid named int field")
 var ErrInvalidByteOrderType = errors.New("invalid byte order type, expected [2]byte")
 var ErrInvalidByteOrder = errors.New("invalid byte order, expected 0xFEFF or 0xFFFE")
 var ErrUnknownString = errors.New("unknown string type")
@@ -54,62 +32,129 @@ var ErrInvalidFixedPointBits = errors.New("invalid fixed point bits, bit counts 
 var ErrInvalidFixedPointSign = errors.New("invalid fixed point sign, sign can only be 0 or 1 bit")
 var ErrInvalidFixedPointType = errors.New("invalid fixed point type, expected float32 or float64")
 var ErrNotSeeker = errors.New("reader does not implement io.Seeker")
-var ErrInvalidSeekposType = errors.New("invalid seekpos type, expected integer")
+var ErrInvalidTellType = errors.New("invalid seekpos type, expected integer")
 
 var SignatureLE = [2]byte{0xFF, 0xFE}
 var SignatureBE = [2]byte{0xFE, 0xFF}
 
-func decodeValue(t reflect.Value, r *EndianedReader, tags decodeTags) {
+func Decode[T any](r io.Reader) (v T, err error) {
+	defer util.Recover(&err)
+
+	d := newDecoder(r)
+
+	// Simple things
+	t := reflect.New(reflect.TypeFor[T]()).Elem()
+	d.decodeValue(t, "")
+	v = t.Interface().(T)
+	return
+}
+
+type decoder struct {
+	*EndianedReader                  // Source file
+	io.Seeker                        // The seeker of reader's internal reader, may be nil
+	namedInts       map[string]int64 // Named ints
+	offset          int64            // Struct start offset
+}
+
+func newDecoder(r io.Reader) (out decoder) {
+	out.EndianedReader = &EndianedReader{
+		Reader:    r,
+		ByteOrder: getEndian(r),
+	}
+
+	out.namedInts = make(map[string]int64)
+	out.Seeker, _ = r.(io.Seeker)
+	if out.Seeker != nil {
+		out.offset, _ = At[int64](out.Seeker)
+		out.namedInts["root"] = out.offset
+	}
+
+	return
+}
+
+func (d *decoder) clone() *decoder {
+	c := new(decoder)
+	*c = *d
+
+	c.namedInts = maps.Clone(d.namedInts)
+	if c.Seeker != nil {
+		c.offset, _ = At[int64](d)
+	}
+
+	return c
+}
+
+// Tries to read an int.
+// It tries the following, in this order:
+//
+// 1. Parse name as int
+// 2. Get int from named fields
+// 3. Read int from stream using named type
+//
+// If all of these fail, the method panics
+func (d *decoder) getInt(intName string) int64 {
+	if intval, err := strconv.ParseInt(intName, 0, 64); err == nil {
+		return intval
+	} else if seenInt, ok := d.namedInts[intName]; ok {
+		return seenInt
+	} else if intVal, ok := d.readIntFromNamedType(intName); ok {
+		return intVal
+	} else {
+		panic(ErrUnknownNamedInt)
+	}
+}
+
+func (d *decoder) decodeValue(t reflect.Value, tags reflect.StructTag) {
 	switch t.Kind() {
 
 	// Basic kinds
 	case reflect.Uint8:
-		val := uint64(ReadSingle[uint8](r))
+		val := uint64(ReadSingle[uint8](d))
 		if t.CanSet() {
 			t.SetUint(val)
 		}
 	case reflect.Uint16:
-		val := uint64(ReadSingle[uint16](r))
+		val := uint64(ReadSingle[uint16](d))
 		if t.CanSet() {
 			t.SetUint(val)
 		}
 	case reflect.Uint32:
-		val := uint64(ReadSingle[uint32](r))
+		val := uint64(ReadSingle[uint32](d))
 		if t.CanSet() {
 			t.SetUint(val)
 		}
 	case reflect.Uint64:
-		val := uint64(ReadSingle[uint64](r))
+		val := uint64(ReadSingle[uint64](d))
 		if t.CanSet() {
 			t.SetUint(val)
 		}
 	case reflect.Int8:
-		val := int64(ReadSingle[int8](r))
+		val := int64(ReadSingle[int8](d))
 		if t.CanSet() {
 			t.SetInt(val)
 		}
 	case reflect.Int16:
-		val := int64(ReadSingle[int16](r))
+		val := int64(ReadSingle[int16](d))
 		if t.CanSet() {
 			t.SetInt(val)
 		}
 	case reflect.Int32:
-		val := int64(ReadSingle[int32](r))
+		val := int64(ReadSingle[int32](d))
 		if t.CanSet() {
 			t.SetInt(val)
 		}
 	case reflect.Int64:
-		val := int64(ReadSingle[int64](r))
+		val := int64(ReadSingle[int64](d))
 		if t.CanSet() {
 			t.SetInt(val)
 		}
 	case reflect.Float32:
-		val := decodeFloat[float32](r, tags)
+		val := decodeFloat[float32](d, tags)
 		if t.CanSet() {
 			t.SetFloat(val)
 		}
 	case reflect.Float64:
-		val := decodeFloat[float64](r, tags)
+		val := decodeFloat[float64](d, tags)
 		if t.CanSet() {
 			t.SetFloat(val)
 		}
@@ -120,9 +165,14 @@ func decodeValue(t reflect.Value, r *EndianedReader, tags decodeTags) {
 		switch {
 		case typ.Implements(reflect.TypeFor[binary.ByteOrder]()):
 			t2 := reflect.New(reflect.TypeFor[[2]byte]()).Elem()
-			decodeValue(t2, r, tags)
+			d.decodeValue(t2, tags)
 			newEndian := GetEndianFromSignature(t2.Interface().([2]byte))
 			t.Set(reflect.ValueOf(newEndian))
+
+			// Set byte order?
+			if _, ok := tags.Lookup(tagByteorder); ok {
+				d.ByteOrder = newEndian
+			}
 
 		default:
 			panic("cannot decode this kind of type")
@@ -130,130 +180,185 @@ func decodeValue(t reflect.Value, r *EndianedReader, tags decodeTags) {
 
 	// Arrays and slices
 	case reflect.Array:
-		decodeArray(t, r, tags)
+		d.decodeArray(t, tags)
 	case reflect.Slice:
-		if tags.length < 0 {
-			panic(ErrSliceMissingLength)
-		}
-		val := reflect.MakeSlice(t.Type(), tags.length, tags.length)
-
-		tags.length = defaultTags.length
-		decodeArray(val, r, tags)
-		if t.CanSet() {
-			t.Set(val)
-		}
+		d.decodeSlice(t, tags)
 
 	// Other weird ones
 	case reflect.Bool:
-		val := ReadSingle[byte](r) != 0
+		val := ReadSingle[byte](d) != 0
 		if t.CanSet() {
 			t.SetBool(val)
 		}
 	case reflect.Pointer:
 		val := reflect.New(t.Type())
-		decodeValue(val.Elem(), r, tags)
+		d.decodeValue(val.Elem(), tags)
 		if t.CanSet() {
 			t.Set(val)
 		}
 	case reflect.String:
-		decodeString(t, r, tags)
+		d.decodeString(t, tags)
 
 	// Structs
 	case reflect.Struct:
-		decodeStruct(t, r)
+		d.decodeStruct(t)
 
 	default:
 		panic("cannot deserialize this kind of type")
 	}
 }
 
-func decodeArray(t reflect.Value, r *EndianedReader, tags decodeTags) {
-	elems := t.Len()
-	for i := range elems {
-		decodeValue(t.Index(i), r, tags)
+func (d *decoder) decodeSlice(t reflect.Value, tags reflect.StructTag) {
+	lengthTag, ok := tags.Lookup(tagLength)
+	if !ok {
+		panic(ErrSliceMissingLength)
+	}
+
+	length := int(d.getInt(lengthTag))
+	val := reflect.MakeSlice(t.Type(), length, length)
+	d.decodeArray(val, tags)
+	if t.CanSet() {
+		t.Set(val)
 	}
 }
 
-func decodeStruct(t reflect.Value, r *EndianedReader) {
-	numFields := t.NumField()
-	seenInts := make(map[string]int)
+func (d *decoder) decodeArray(t reflect.Value, tags reflect.StructTag) {
+	elems := t.Len()
+	for i := range elems {
+		d.decodeValue(t.Index(i), tags)
+	}
 
+	// Set byte order?
+	if _, ok := tags.Lookup(tagByteorder); ok {
+		indir := reflect.Indirect(t)
+		if bytes, ok := indir.Interface().([2]byte); ok {
+			d.ByteOrder = GetEndianFromSignature(bytes)
+		} else {
+			panic(ErrInvalidByteOrderType)
+		}
+	}
+}
+
+func (d *decoder) readIntFromNamedType(intTypeName string) (int64, bool) {
+	// Read int from file instead
+	intKind, ok := intKindFromName(intTypeName)
+	if !ok {
+		return 0, false
+	}
+	intValue := reflect.New(intTypeFromKind(intKind)).Elem()
+	d.decodeValue(intValue, "")
+
+	if intValue.CanInt() {
+		return int64(intValue.Int()), true
+	} else if intValue.CanUint() {
+		return int64(intValue.Uint()), true
+	} else {
+		panic("cannot get int, because int type cannot int or uint. huh???")
+	}
+}
+
+func (d *decoder) decodeStruct(t reflect.Value) {
+	d = d.clone()
+
+	numFields := t.NumField()
 	for i := range numFields {
 		fieldType := t.Type().Field(i)
 		field := t.Field(i)
 
-		// Seek tag?
-		if _, ok := fieldType.Tag.Lookup(tagSeekpos); ok {
-			seeker, ok := r.Reader.(io.Seeker)
-			if !ok {
-				panic(ErrNotSeeker)
-			}
-			at := util.Must1(At[int64](seeker))
-			if field.CanUint() {
-				field.SetUint(uint64(at))
-			} else if field.CanInt() {
-				field.SetInt(at)
-			} else {
-				panic(ErrInvalidSeekposType)
-			}
-			continue
-		}
-
-		// Read tags
-		tags := defaultTags
-		if lengthField, ok := fieldType.Tag.Lookup(tagLength); ok {
-			if intval, err := strconv.ParseInt(lengthField, 0, 64); err == nil {
-				tags.length = int(intval)
-			} else if length, ok := seenInts[lengthField]; ok {
-				tags.length = length
-			} else {
-				panic(ErrUnknownLengthProperty)
-			}
-		}
-		if stringType, ok := fieldType.Tag.Lookup(tagString); ok {
-			tags.xstring = stringType
-		}
-		if fixedType, ok := fieldType.Tag.Lookup(tagFixedPoint); ok {
-			tags.fixed = fixedType
-		}
-
-		// Decode value of field
-		decodeValue(field, r, tags)
-
-		// Save integer, maybe
-		indir := reflect.Indirect(field)
-		if indir.CanInt() {
-			seenInts[fieldType.Name] = int(indir.Int())
-		} else if indir.CanUint() {
-			seenInts[fieldType.Name] = int(indir.Uint())
-		}
-
-		// Set byte order?
-		if _, ok := fieldType.Tag.Lookup(tagByteorder); ok {
-			if bytes, ok := indir.Interface().([2]byte); ok {
-				r.ByteOrder = GetEndianFromSignature(bytes)
-			} else if bo, ok := indir.Interface().(binary.ByteOrder); ok {
-				r.ByteOrder = bo
-			} else {
-				panic(ErrInvalidByteOrderType)
-			}
-		}
+		d.decodeField(field, fieldType)
 	}
 }
 
-func decodeString(t reflect.Value, r *EndianedReader, tags decodeTags) {
-	str := ""
+func (d *decoder) decodeField(field reflect.Value, fieldType reflect.StructField) {
+	// Tell tag?
+	if tellName, ok := fieldType.Tag.Lookup(tagTell); ok {
+		d.decodeTell(field, tellName)
+		return
+	}
 
-	switch tags.xstring {
-	case "ascii":
-		var sr io.Reader = r
-		if tags.length >= 0 {
-			dat := make([]byte, tags.length)
-			util.Must1(io.ReadFull(r, dat))
-			sr = bytes.NewReader(dat)
+	// Offset tag, move position
+	if offsetType, ok := fieldType.Tag.Lookup(tagOffset); ok {
+		if d.Seeker == nil {
+			panic(ErrNotSeeker)
 		}
+
+		// Get data offset
+		offset := d.getInt(offsetType)
+
+		// Save current offset and set new offset
+		posBefore := util.Must1(At[int64](d.Seeker))
+		util.Must1(d.Seeker.Seek(d.offset+offset, io.SeekStart))
+
+		defer func() {
+			util.Must1(d.Seeker.Seek(posBefore, io.SeekStart))
+		}()
+	}
+
+	// Decode value of field
+	d.decodeValue(field, fieldType.Tag)
+
+	// Save integer, maybe
+	indir := reflect.Indirect(field)
+	if indir.CanInt() {
+		d.namedInts[fieldType.Name] = indir.Int()
+	} else if indir.CanUint() {
+		d.namedInts[fieldType.Name] = int64(indir.Uint())
+	}
+}
+
+func (d *decoder) decodeTell(field reflect.Value, tellName string) {
+	if d.Seeker == nil {
+		panic(ErrNotSeeker)
+	}
+	offset := util.Must1(At[int64](d.Seeker))
+
+	// Set field, if possible
+	if field.CanUint() {
+		field.SetUint(uint64(offset))
+		d.namedInts[field.Type().Name()] = offset
+	} else if field.CanInt() {
+		field.SetInt(offset)
+		d.namedInts[field.Type().Name()] = offset
+	}
+
+	// Register specified tell name
+	if tellName != "" {
+		d.namedInts[tellName] = offset
+	}
+}
+
+func (d *decoder) decodeString(t reflect.Value, tags reflect.StructTag) {
+
+	// Validate string tag
+	stringTag, ok := tags.Lookup(tagString)
+	if !ok {
+		panic("no string tag provided")
+	}
+	parts := strings.SplitN(stringTag, ",", 2)
+	if len(parts) == 0 {
+		panic("malformed string tag")
+	}
+
+	stringType := parts[0]
+
+	// Do we read string from a fixed-length buffer?
+	stringReader := io.Reader(d)
+	if len(parts) == 2 {
+		stringLength := d.getInt(parts[2])
+		if stringLength < 0 {
+			panic("string cannot have length < 0")
+		}
+
+		dat := make([]byte, stringLength)
+		util.Must1(io.ReadFull(d, dat))
+		stringReader = bytes.NewReader(dat)
+	}
+
+	str := ""
+	switch stringType {
+	case "ascii":
 		for {
-			b := ReadSingle[byte](sr)
+			b := ReadSingle[byte](stringReader)
 			if b != 0 {
 				str += string(rune(b))
 			} else {
@@ -271,15 +376,16 @@ func decodeString(t reflect.Value, r *EndianedReader, tags decodeTags) {
 	}
 }
 
-func decodeFloat[T float32 | float64](r *EndianedReader, tags decodeTags) float64 {
-	if tags.fixed != "" {
-		return decodeFixedPoint(r, tags.fixed)
+// Go I'm begging you, PLEASE let me make generic methods!
+func decodeFloat[T float32 | float64](d *decoder, tags reflect.StructTag) float64 {
+	if fixedType, ok := tags.Lookup(tagFixedPoint); ok {
+		return d.decodeFixedPoint(fixedType)
 	} else {
-		return float64(ReadSingle[T](r))
+		return float64(ReadSingle[T](d))
 	}
 }
 
-func decodeFixedPoint(r *EndianedReader, fixedTag string) float64 {
+func (d *decoder) decodeFixedPoint(fixedTag string) float64 {
 	bits := strings.Split(fixedTag, ",")
 	if len(bits) != 3 {
 		panic(ErrInvalidFixedPointSignature)
@@ -312,7 +418,7 @@ func decodeFixedPoint(r *EndianedReader, fixedTag string) float64 {
 	// Read bits
 	rawData := uint64(0)
 	rawBytes := make([]byte, totalBits/8)
-	util.Must1(r.ReadWithOrder(rawBytes))
+	util.Must1(d.ReadWithOrder(rawBytes))
 	for i := range rawBytes {
 		rawData |= uint64(rawBytes[i]) << ((len(rawBytes) - (i + 1)) * 8)
 	}
@@ -339,11 +445,61 @@ func decodeFixedPoint(r *EndianedReader, fixedTag string) float64 {
 }
 
 func GetEndianFromSignature(signature [2]byte) binary.ByteOrder {
-	if signature == SignatureBE {
+	switch signature {
+	case SignatureBE:
 		return binary.BigEndian
-	} else if signature == SignatureLE {
+	case SignatureLE:
 		return binary.LittleEndian
-	} else {
+	default:
 		panic(ErrInvalidByteOrder)
+	}
+}
+
+func intKindFromName(name string) (reflect.Kind, bool) {
+	switch strings.ToLower(name) {
+	case "u8", "uint8":
+		return reflect.Uint8, true
+	case "u16", "uint16":
+		return reflect.Uint16, true
+	case "u32", "uint32":
+		return reflect.Uint32, true
+	case "u64", "uint64":
+		return reflect.Uint64, true
+
+	case "s8", "i8", "int8":
+		return reflect.Int8, true
+	case "s16", "i16", "int16":
+		return reflect.Int16, true
+	case "s32", "i32", "int32":
+		return reflect.Int32, true
+	case "s64", "i64", "int64":
+		return reflect.Int64, true
+
+	default:
+		return reflect.Invalid, false
+	}
+}
+
+func intTypeFromKind(kind reflect.Kind) reflect.Type {
+	switch kind {
+	case reflect.Uint8:
+		return reflect.TypeFor[uint8]()
+	case reflect.Uint16:
+		return reflect.TypeFor[uint16]()
+	case reflect.Uint32:
+		return reflect.TypeFor[uint32]()
+	case reflect.Uint64:
+		return reflect.TypeFor[uint64]()
+	case reflect.Int8:
+		return reflect.TypeFor[int8]()
+	case reflect.Int16:
+		return reflect.TypeFor[int16]()
+	case reflect.Int32:
+		return reflect.TypeFor[int32]()
+	case reflect.Int64:
+		return reflect.TypeFor[int64]()
+
+	default:
+		panic("non-binary int type")
 	}
 }
